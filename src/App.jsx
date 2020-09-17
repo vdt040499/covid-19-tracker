9960ce55e76df2db179fd9f574f3c515eb94bf7b
VoTan <vdt040499@gmail.com>
2020-09-17 20:37:51 +0700

    Chart

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 import InfoBox from "./components/InfoBox";
+import LineGraph from "./components/LineGraph";
 import Map from "./components/Map";
 import Table from "./components/Table";
 import { sortData } from "./util";
@@ -115,8 +116,8 @@ function App() {
           <h3>Live Cases by Country</h3>
           <Table countries={tableData}/>
 
-          <h3>Worldwide new cases</h3>
-          {/* Graph */}
+          <h3 className="app__titleChart">Worldwide new cases</h3>
+          <LineGraph />
         </CardContent>
       </Card>
     </div>
